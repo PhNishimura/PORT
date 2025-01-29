55ed0d985f766e515069ef2fe98fb044431ae5cd
Pedro Nishimura <129108608+PhNishimura@users.noreply.github.com>
2025-01-29 07:35:17 -0300

    rolagem

--- styles/java.js
+++ styles/java.js
@@ -2,3 +2,24 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
     var navList = document.getElementById('nav-list');
     navList.classList.toggle('active');
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+    const checkVisibility = () => {
+        portfolioItems.forEach(item => {
+            const itemTop = item.getBoundingClientRect().top;
+            const itemBottom = item.getBoundingClientRect().bottom;
+
+            if (itemTop < window.innerHeight && itemBottom > 0) {
+                item.classList.add('visible');
+            }
+        });
+    };
+
+
+    window.addEventListener('scroll', checkVisibility);
+    checkVisibility(); 
+});
